44fe23d63d45fafbf1261a08ece5a1ec6a6c0514
sathyan1212 <114426501+sathyan1212@users.noreply.github.com>
2022-10-16 17:02:25 +0530

    Update Delete Note.jsx

--- project/delete/Delete Note.jsx
+++ project/delete/Delete Note.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useContext } from 'react';
 
 import { Card, CardContent, CardActions, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
